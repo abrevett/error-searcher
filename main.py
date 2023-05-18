946819482ee196eac162c09b5bad16534ed7f0ec
abrevett <abrevett@udel.edu>
2023-05-18 14:35:29 -0400

    Finished STackExchange API link listing

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import itertools
 
 STACKX_SITES = {}
 
+forums = [ "serverfault", "stackoverflow", "unix", "askubuntu", "superuser" ]
 stackx_query = {
     "pagesize": 100, # Maximum Size per page
     "order": "desc",
@@ -14,17 +15,42 @@ stackx_query = {
     "filter": "!.kZ-f.vMXeIXqR48NteY"
 }
 
-def serp_search():
+serp_query = {
+    "filter": 0,
+    "nfpr": 1,
+    "num": 10000,
+}
+
+def serp_search(payload: dict) -> dict:
+    search = GoogleSearch(payload)
+#    print(search.get_dict())
+    return search.get_dict()["organic_results"]
+
+def serp_run(query):
     key = ""
     with open("serpapi.key", "r") as keyfile:
         key = keyfile.read()
+    
+    payload = dict(serp_query)
+    payload.update({"q": query,"serp_api_key": key})
+    print(payload)
+    search = GoogleSearch(payload)
+    organic = search.get_dict()
+    organic = organic["organic_results"]
+    # Create link-html mappings just like StackExchange:
+    ret = {}
+    for link in organic:
+        req = requests.get(link["link"])
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        # get all <p> tags, which probably have regex we need
+        text = ""
+        for p in soup.find_all("p"):
+            text += soup.get_text()
+        ret.append({ "link": link, "body": text })
+    return ret
 
-    search = GoogleSearch({
-            "q": "alpine linux",
-            "serp_api_key": key
-        })
 
-    return search.get_dict()
 
 def google_search():
     key = ""
@@ -67,19 +93,18 @@ def stackx_run(query):
     key = ""
     with open("stackx.key", "r") as keyfile:
         key = keyfile.read().strip()
-    forums = [ "serverfault", "stackoverflow", "unix", "askubuntu", "superuser" ]
     resp = {}
     for site in forums:
         pagenum = 1
         payload = dict(stackx_query) 
         payload.update({"key": key, "page": pagenum, "q": query, "site": site})
-        print(stackx_query)
-        print(payload)
+#        print(stackx_query)
+#        print(payload)
         resp[site] = {}
 
         resp[site][pagenum] = stackx_search(payload)
         while( resp[site][pagenum]["has_more"] == True ):
-            print("getting more!")
+#            print("getting more!")
             pagenum += 1
             payload.update({ "page": pagenum })
             resp[site][pagenum] = stackx_search(payload)
@@ -126,15 +151,16 @@ def main():
     regexs = [ re.compile( r".*" + rx + r".*", flags=re.I) for rx in mapping_ind ]
 
     result = stackx_run(user.strip())
-    with open("result_dump.json", "w+") as jsdump:
-        jsdump.write( json.dumps(result, indent=2) )
+    with open("stackx_dump.json", "w+") as jsdump:
+        jsdump.write( json.dumps(result) )
     print("results written to result_dump.json")
 
 #    result = {}
-#    with open("result_dump.json", "r") as jsdump:
+#    with open("stackx_dump.json", "r") as jsdump:
 #        result = json.loads(jsdump.read())
     
     # We now iterate through each response and search for our regex pattern
+    # StackExchange iteration
     relevant = []
     for forum in result.keys():
         site = result[forum]
@@ -149,11 +175,30 @@ def main():
                     item["weight"] = weight
                     relevant.append(item)
                     i += 1
-    
+
+    old = """
+    # SerpAPI searching:
+    serppy = serp_run(user.strip())
+    with open("serp_dump.json", "w+") as serpdump:
+        serpdump.write( json.dumps(serppy) )
+
+    for link in serppy:
+        i=0
+        weight=0
+        for regex in regexs:
+            if regex.search(link["body"]) and mapping[mapping_ind[i]] > weight:
+                weight = mapping[mapping_ind[i]]
+            link["weight"] = weight
+            relevant.append(link)
+            i += 1
+    """
+
+    # We sort and filter out links that are unrelated:
     relevant = [ it for it in relevant if it["weight"] > 0.3 ]
-    relevant.sort( key=lambda x: x["weight"])
+    relevant.sort( key=lambda x: x["weight"], reverse=True)
     rel_links = set([ it["link"] for it in relevant ])
-    print(rel_links)
-    print( "StackExchange: " + str(len(rel_links)) )
+    print( "StackExchange: " + str(len(rel_links)) + "\n" )
+    for link in rel_links:
+        print( link)
 
 main()
